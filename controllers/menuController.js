0e4d65c97ffc64508f2a883668e140ed0c62234c
Jae <gjwodud119@gmail.com>
2022-05-09 12:16:26 +0900

    추가: Menu Controller 기본 구조 추가

---
+++ controllers/menuController.js
@@ -0,0 +1,23 @@
+import {} from "../"
+
+const getMenu = async (req, res) => {
+
+}
+
+const getMenuType = async (req, res) => {
+
+}
+
+const recommendMenu = async (req, res) => {
+
+}
+
+const uploadMenu = async (req, res) => {
+
+}
+
+const updateMenu = async (req, res) => {
+
+}
+
+export {getMenu, getMenuType, recommendMenu, uploadMenu, updateMenu}
