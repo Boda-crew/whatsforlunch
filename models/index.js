088c3c3631269441a0a38b20629e0bd4f9ee0db7
Jae <gjwodud119@gmail.com>
2022-05-09 18:16:25 +0900

    추가: 메뉴 모델 인덱싱

--- models/index.js
+++ models/index.js
@@ -1,7 +1,16 @@
 import Sequelize from "sequelize";
 import configFile from "../config/config.js"
+import {Menu} from "./Menu.js";
 
 const env = process.env.NODE_ENV || 'dev';
 const config = configFile[env];
+export const db = {};
 
-export const sequelize = new Sequelize(config.database, config.username, config.password, config)
+const sequelize = new Sequelize(config.database, config.username, config.password, config)
+
+db.sequelize = sequelize;
+
+db.Menu = Menu;
+
+Menu.init(sequelize);
+Menu.associate(db);
