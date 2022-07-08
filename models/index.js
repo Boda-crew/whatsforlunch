88e6493ebbf84c186775f1ad6a28af5c1ed00e5e
jae-heo <gjwodud119@gmail.com>
2022-07-08 10:05:05 +0900

    변경: db export 를 default 로 변경/ db config 가독성 향상

--- models/index.js
+++ models/index.js
@@ -4,13 +4,14 @@ import {Menu} from "./Menu.js";
 
 const env = process.env.NODE_ENV || 'dev';
 const config = configFile[env];
-export const db = {};
-
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
-db.sequelize = sequelize;
-
-db.Menu = Menu;
+const db = {
+    sequelize,
+    Menu
+};
 
 Menu.init(sequelize);
 Menu.associate(db);
+
+export default db
