f738d101cebfeda73eedb7adfcfafb85efc2e7eb
jae-heo <gjwodud119@gmail.com>
2022-07-08 13:36:30 +0900

    추가: winston 을 이용한 로깅 기능 추가

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import express from "express";
 import db from "./models/index.js";
 import menuRoutes from './routes/menuRoute.js';
+import logger from "./config/winston.js"
 
 const app = express();
 let port = process.env.port || 8080;
 
 db.sequelize.sync({force: false })
     .then(() => {
-        console.log("DB Connected");
+        logger.info("DB connected")
     }).catch((err) => {
-        //에러처리
+        logger.error(err)
     })
 
 app.use(express.json());
@@ -21,8 +22,9 @@ app.use((req, res) => {
     res.status(404).json({
         message: "잘못된 요청입니다."
     });
+    logger.error("잘못된 요청입니다.")
 });
 
 app.listen(port, () => {
-    console.log(`server on ${port}`);
+    logger.info(`server on ${port}`);
 });
